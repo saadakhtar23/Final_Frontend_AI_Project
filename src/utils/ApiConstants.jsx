06f0dfd6645f4f6a11c9e6f1a33e2b1c9a9d1faf
AdityaRaj <aditya@netfotech.in>
2026-01-13 11:29:07 +0530

    hmm

--- src/utils/ApiConstants.jsx
+++ src/utils/ApiConstants.jsx
@@ -1,5 +1,5 @@
-// export const baseUrl = "https://final-adminbackend-ai-project.onrender.com"
-export const baseUrl = "http://localhost:4000"
+export const baseUrl = "https://final-adminbackend-ai-project.onrender.com"
+// export const baseUrl = "http://localhost:4000"
 export const superAdminBaseUrl = "https://multi-tenant-website-backend.onrender.com"
 // export const superAdminBaseUrl = "http://localhost:6000"
 export const pythonUrl ="https://python-k0xt.onrender.com"
